78495f06439082a62d2aae76a470e87539fe7668
Luca Bonamini <lucabonamini28@gmail.com>
2020-12-19 16:47:12 +0100

    Working on genetic algorithm for PID tuning

--- tests/ga.cpp
+++ tests/ga.cpp
@@ -2,32 +2,21 @@
 #include "ga.h"
 #include <random>
 #include <memory>
-#include "matplotlibcpp.h"
+#include <chrono>
 
-constexpr int MAX_GENERATION = 1;
-constexpr int MAX_ITERATION = 2500;
+constexpr int MAX_GENERATION = 300;
+constexpr int MAX_ITERATION = 4500;
+constexpr double KP_MAX = 20.0;
+constexpr double KI_MAX = 1.0;
+constexpr double KD_MAX = 20.0;
 
-double round(double var) 
-{ 
-    // 37.66666 * 100 =3766.66 
-    // 3766.66 + .5 =3767.16    for rounding off value 
-    // then type cast to int so value is 3767 
-    // then divided by 100 so the value converted into 37.67 
-    double value = (int)(var * 100 + .5); 
-    return (double)value / 100; 
-} 
 
 int main(int argc, char** argv) {
 
-    std::mt19937 rng;
-    rng.seed(std::random_device{}());
-    std::uniform_real_distribution<double> dist_p(0.0,20.0);
-    std::uniform_real_distribution<double> dist_i(0.0,1.0);
-    std::uniform_real_distribution<double> dist_d(0.0,20.0);
-
     std::vector<double> wx {-2.5,0.0,2.5,5.0,7.5,3.0,-1.0};
     std::vector<double> wy {0.7,-6.0,2.0,-4.0,0.0,5.0,-2.0};
-    std::vector<State> states;
+
+    std::vector<std::vector<State>> states;
     std::vector<double> rx,ry,ryaw;
     CubicSplinePlanner::Spline2D csp(wx,wy);
     for (double i=0; i<csp.s.back(); i+=0.1) {
@@ -37,16 +26,22 @@ int main(int argc, char** argv) {
         ryaw.push_back(csp.calc_yaw(i));
     }
 
-    // Initial conditions
     State init;
     init.x = rx.front();
     init.y = ry.front();
     init.theta = std::atan2((ry.at(1)-ry.at(0)),(rx.at(1)-rx.at(0)));
     init.v = 0.1;
     std::unique_ptr<RobotModel> rm;
-    int generation = 0;
+    int generation = 1;
     Population pop;
     pop.chromosomes.resize(100);
+
+    std::mt19937 rng;
+    rng.seed(std::random_device{}());
+    std::uniform_real_distribution<double> dist_p(0.0,20.0);
+    std::uniform_real_distribution<double> dist_i(0.0,1.0);
+    std::uniform_real_distribution<double> dist_d(0.0,20.0);
+
     for (size_t i = 0; i < pop.chromosomes.size(); i++) {
         pop.chromosomes.at(i).Kp = dist_p(rng);
         pop.chromosomes.at(i).Ki = dist_i(rng)*0.00001;
@@ -56,16 +51,15 @@ int main(int argc, char** argv) {
 
     std::unique_ptr<Pid> pid;
     std::vector<double> offtrack_errors;
-    std::vector<double> fitness_values;
+    std::vector<Chromosome> best_gains;
     bool goal = false;
 
     while (generation < MAX_GENERATION) {
-        generation++;
-        int iteration = 0;
         for (size_t i = 0; i < pop.chromosomes.size(); i++) {
-            iteration = 0;
+            auto start = std::chrono::high_resolution_clock::now();
+            int iteration = 0;
             offtrack_errors.clear();
-            states.clear();
+            std::vector<State> single_run;
             while (iteration < MAX_ITERATION) {
                 if (iteration == 0) {
                     rm = std::make_unique<RobotModel>(init);
@@ -74,64 +68,77 @@ int main(int argc, char** argv) {
                         pop.chromosomes.at(i).Kd);
                 }
                 double velocity = 0.1;
+                auto stop = std::chrono::high_resolution_clock::now();
+                auto duration = duration_cast<std::chrono::microseconds>(stop - start);
                 auto track_error = findClosestIndex(pid->closest_index,rm->state_,rx,ry);
+                stop = std::chrono::high_resolution_clock::now();
+                duration = duration_cast<std::chrono::microseconds>(stop - start);
+
                 pid->closest_index = track_error.first;
-                
+
                 auto steer = pid->calcError(rm->state_,track_error,ryaw);
 
-                if (steer < -30*M_PI/180) {
-                    steer = -30*M_PI/180;
-                } else if (steer > 30*M_PI/180) {
-                    steer = 30*M_PI/180; 
+                if (steer < -60*M_PI/180) {
+                    steer = -60*M_PI/180;
+                } else if (steer > 60  *M_PI/180) {
+                    steer = 60 *M_PI/180;
                 }
                 rm->updateState(steer,velocity);
 
-                auto dist_from_goal = std::sqrt((rm->state_.x - rx.back())*(rm->state_.x - rx.back()) + (rm->state_.y - ry.back())*(rm->state_.y - ry.back()));
-                offtrack_errors.push_back(track_error.second);
-                // std::cout << "dist_from_goal: " << dist_from_goal << std::endl;
+                auto dist_from_goal = std::sqrt((rm->state_.x - rx.back())*(rm->state_.x - rx.back()) +
+                    (rm->state_.y - ry.back())*(rm->state_.y - ry.back()));
+                double curr_closest_dist = std::sqrt((rm->state_.x - rx.at(track_error.first))*(rm->state_.x - rx.at(track_error.first)) +
+                    (rm->state_.y - ry.at(track_error.first))*(rm->state_.y - ry.at(track_error.first)));
+                offtrack_errors.push_back(curr_closest_dist);
+
                 if (dist_from_goal < 0.1) {
-                    std::cout << "==== GOAL ====" << std::endl;
                     goal = true;
                     break;
                 }
-                states.push_back(rm->state_);
+                single_run.push_back(rm->state_);
                 iteration++;
+                stop = std::chrono::high_resolution_clock::now();
+                duration = duration_cast<std::chrono::microseconds>(stop - start);
             }
-            double value = 0.0;
-            for (auto err:offtrack_errors) {
-                value+=abs(err);
-            }
-            pop.chromosomes.at(i).fitness = value;
-            // if (goal) {
-                fitness_values.push_back(pop.chromosomes.at(i).fitness);
-            // } else {
-            //     fitness_values.push_back(1.0);
-            // }
-            std::cout << "i: " << i << " , "
-                << "goal: " << goal << " , "
-                << "Kp: " << pop.chromosomes.at(i).Kp << " , "
-                << "Ki: " << pop.chromosomes.at(i).Ki << " , "
-                << "Kd: " << pop.chromosomes.at(i).Kd << " , "
-                << "fitness: " << fitness_values.back() << std::endl;
+            pid.reset();
+            rm.reset();
+            states.push_back(single_run);
+            pop.chromosomes.at(i).fitness = *std::max_element(offtrack_errors.begin(),offtrack_errors.end());
         }
         std::stable_sort(pop.chromosomes.begin(),pop.chromosomes.end(), [](const auto& a, const auto& b) {
             return  a.fitness < b.fitness;
         });
         std::vector<Chromosome> parents {pop.chromosomes.at(0),pop.chromosomes.at(1)};
+
+        std::cout << "############  FITNESS: "<< " " << pop.chromosomes.at(0).fitness << "############" << std::endl;
+
+        best_gains.push_back(pop.chromosomes.at(0));
+        for (size_t i = 0; i < pop.chromosomes.size(); i++) {
+            double cross_idx = static_cast<double> (rand()) / static_cast<double> (RAND_MAX); // crossover index
+            double cross_kp = cross_idx * parents.at(0).Kp + (1-cross_idx) * parents.at(1).Kp;
+            double cross_ki = cross_idx * parents.at(0).Ki + (1-cross_idx) * parents.at(1).Ki;
+            double cross_kd = cross_idx * parents.at(0).Kd + (1-cross_idx) * parents.at(1).Kd;
+            double mut_idx = static_cast<double> (rand()) / static_cast<double> (RAND_MAX);
+            double mut_kp = cross_kp + (KP_MAX*mut_idx);
+            double mut_ki = cross_ki + (KI_MAX*mut_idx*0.00001);
+            double mut_kd = cross_kd + (KD_MAX*mut_idx);
+            pop.chromosomes.at(i).Kp = mut_kp;
+            pop.chromosomes.at(i).Ki = mut_ki;
+            pop.chromosomes.at(i).Kd = mut_kd;
+            pop.chromosomes.at(i).fitness = 0.0;
+        }
+        generation++;
     }
 
-    std::cout << "Best fitness: " << *std::min_element(fitness_values.begin(),fitness_values.end()) << std::endl;
+    std::stable_sort(best_gains.begin(),best_gains.end(), [](const auto& a, const auto& b) {
+            return  a.fitness < b.fitness;
+    });
 
-    // std::vector<double> state_x,state_y;
-    // for (auto state:states) {
-    //     state_x.push_back(state.x);
-    //     state_y.push_back(state.y);
-    // }
-    // matplotlibcpp::plot(rx,ry,"-k");
-    // matplotlibcpp::plot(wx,wy,"ob");
-    // matplotlibcpp::plot(state_x,state_y,"xr");
-    // matplotlibcpp::title("PID Controller");
-    // matplotlibcpp::show();
+    std::cout << "Best fitness: " << best_gains.at(0).fitness << std::endl;
+    std::cout << "Best chromosome: " << best_gains.at(0).Kp << " , "
+        << best_gains.at(0).Ki << " , "
+        << best_gains.at(0).Kd << " , "
+        << best_gains.at(0).fitness << std::endl;
 
     return 0;
 }
--- tests/ga.h
+++ tests/ga.h
@@ -1,6 +1,8 @@
 #ifndef _GA_H_
 #define _GA_H_
 
+#include "pid/pid.h"
+
 struct Chromosome {
     double Kp;
     double Ki;
