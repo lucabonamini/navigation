bed9104184c28139e62a367fa90aa8b64e1ab130
Luca Bonamini <lucabonamini28@gmail.com>
2021-02-02 23:05:24 +0100

    Remove unnecessary files && Fix reference yaw in example_mpc

--- tests/example_mpc.cpp
+++ tests/example_mpc.cpp
@@ -6,12 +6,27 @@ using namespace matplotlibcpp;
 
 constexpr int MAX_TIME = 5000;
 
+void smoothYaw(std::vector<double> &cyaw){
+  for(size_t i=0; i<cyaw.size()-1; i++){
+    double dyaw = cyaw[i+1] - cyaw[i];
+
+    while (dyaw > M_PI/2.0){
+      cyaw[i+1] -= M_PI*2.0;
+      dyaw = cyaw[i+1] - cyaw[i];
+    }
+    while (dyaw < -M_PI/2.0){
+      cyaw[i+1] += M_PI*2.0;
+      dyaw = cyaw[i+1] - cyaw[i];
+    }
+  }
+}
+
 int main(int argc, char** argv) {
 
     // std::vector<double> wx {-2.5,0.0,2.5,5.0,7.5,3.0,-1.0};
     // std::vector<double> wy {0.7,-6.0,2.0,-4.0,0.0,5.0,-2.0};
-    // std::vector<double> wx({0.0, 60.0, 125.0,  50.0,   75.0,  35.0,  -10.0});
-    // std::vector<double> wy({0.0,  0.0,  50.0,  65.0,   30.0,  50.0,  -20.0});
+    std::vector<double> wx({0.0, 60.0, 125.0,  50.0,   75.0,  35.0,  -10.0});
+    std::vector<double> wy({0.0,  0.0,  50.0,  65.0,   30.0,  50.0,  -20.0});
 
     int time = 0;
     std::vector<State> states;
@@ -31,10 +46,10 @@ int main(int argc, char** argv) {
     auto speed_profile = calcSpeedProfile(rs,rk);
     State initial_state {.x=rx.at(0),.y=ry.at(0),.yaw=ryaw.at(0),.v=speed_profile.at(0)};
 
+    smoothYaw(ryaw);
+
     MPC mpc(initial_state,rx,ry,ryaw,rk,rs,speed_profile);
-    if (mpc.run()) {
-        cout << "Simulation complete." << endl;
-    }
+    mpc.run();
 
     vector<double> x,y;
     for (size_t i = 0; i < mpc.states_.size(); i++) {
