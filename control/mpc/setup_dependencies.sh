b18a2d44b434e4a6e1591962014b5daebd94da94
Luca Bonamini <lucabonamini28@gmail.com>
2021-05-18 22:02:01 +0200

    fix: mpc dependencies

--- control/mpc/setup_dependencies.sh
+++ control/mpc/setup_dependencies.sh
@@ -3,6 +3,6 @@
 # Ipopt
 sudo apt install -y gfortran unzip
 wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.7.zip && unzip Ipopt-3.12.7.zip && rm Ipopt-3.12.7.zip
-cd Ipopt-3.12.7 && ./install_ipopt.sh Ipopt-3.12.7
+cd Ipopt-3.12.7 && ../install_ipopt.sh Ipopt-3.12.7
 # CppAD
 sudo apt install -y cppad
